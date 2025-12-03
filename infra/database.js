0215d7334b0c4797680a37008d1185414dac18ff
João Pedro <109301783+JoaoArgento@users.noreply.github.com>
2025-12-03 15:41:23 +0000

    now a postgres client is usable

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,26 @@
 import { Client } from "pg";
 
-function query(queryInfo) {
-  const client = new Client({});
+async function query(queryInfo) {
+  const client = new Client({
+    port: process.env.POSTGRES_PORT,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "production",
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query(queryInfo);
+    return result;
+  } catch (ex) {
+    console.log("Deu erro pai!", ex);
+  } finally {
+    await client.end();
+  }
 }
+
+export default {
+  query: query,
+};
