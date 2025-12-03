19ac1caa91b40959f145802250823fd1d8948215
João Pedro <109301783+JoaoArgento@users.noreply.github.com>
2025-12-03 15:37:26 +0000

    added tailwind

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "./../styles/globals.css";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold">
+      Ta na hora de finalizar UM PROJETO JOAO
+    </h1>
+  );
+}
--- pages/index.js
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1>Ta na hora de finalizar UM PROJETO JOAO</h1>;
-}
